ace257775f8eacfb861c49a7bf608c55ac57686e
dilane3 <komboudilane125@gmail.com>
2024-02-21 16:44:01 +0100

    Implementing export cards feature

--- src/hooks/useLoadSectors.ts
+++ src/hooks/useLoadSectors.ts
@@ -1,6 +1,5 @@
 import { findAllSectors } from "@/api/sector";
 import { Sector } from "@/entities/sector.entity";
-import { AuthState } from "@/gx/signals/auth.signal";
 import { SectorsActions, SectorsState } from "@/gx/signals/sectors.signal";
 import { useActions, useSignal } from "@dilane3/gx";
 import { useEffect } from "react";
@@ -8,7 +7,6 @@ import { toast } from "react-toastify";
 
 export default function useLoadSectors() {
   // Global state
-  const { user } = useSignal<AuthState>("auth");
   const { loading } = useSignal<SectorsState>("sectors");
 
   // Global actions
